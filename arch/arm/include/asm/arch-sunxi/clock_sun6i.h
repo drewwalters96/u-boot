d75078c9868714d33828a3d0840a0127069a9f22
Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
2016-11-03 00:11:30 +0100

    sun50i: PLL11 (DDR1) has 7 bits to encode the factor N
    When encoding frequencies of 1536MHz or higher (corresponding to a
    DRAM clk of 768MHz or higher), all 7 bits of the PLL11 multiplication
    factor N are needed on the A64 (sun50i).
    
    This fixes the clocking setup to ensure that the field does not get
    truncated.
    
    Signed-off-by: Philipp Tomsich <philipp.tomsich@theobroma-systems.com>
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- arch/arm/include/asm/arch-sunxi/clock_sun6i.h
+++ arch/arm/include/asm/arch-sunxi/clock_sun6i.h
@@ -250,7 +250,11 @@ struct sunxi_ccm_reg {
 #define CCM_PLL10_CTRL_LOCK		(0x1 << 28)
 #define CCM_PLL10_CTRL_EN		(0x1 << 31)
 
+#if defined(CONFIG_MACH_SUN50I)
+#define CCM_PLL11_CTRL_N(n)		((((n) - 1) & 0x7f) << 8)
+#else
 #define CCM_PLL11_CTRL_N(n)		((((n) - 1) & 0x3f) << 8)
+#endif
 #define CCM_PLL11_CTRL_SIGMA_DELTA_EN	(0x1 << 24)
 #define CCM_PLL11_CTRL_UPD		(0x1 << 30)
 #define CCM_PLL11_CTRL_EN		(0x1 << 31)
